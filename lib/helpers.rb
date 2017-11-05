58663a1e7e4ebd5ddd6861891f6669590dd4e436
Sam Starling <mail@samstarling.co.uk>
2017-11-05 12:24:29 +0000

    Switch to using created_at attribute, improve date formatting

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,3 +1,7 @@
 use_helper Nanoc::Helpers::Blogging
 use_helper Nanoc::Helpers::LinkTo
 use_helper Nanoc::Helpers::Rendering
+
+def friendly_date_for(post)
+  attribute_to_time(post[:created_at]).strftime("%-d %B %Y")
+end
