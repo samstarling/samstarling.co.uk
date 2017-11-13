7413a86019fd8c72c55f4216b2dbe6d08b7c4531
Sam Starling <mail@samstarling.co.uk>
2017-11-13 16:58:20 +0100

    Add a TIL section

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -5,3 +5,29 @@ use_helper Nanoc::Helpers::Rendering
 def friendly_date_for(post)
   attribute_to_time(post[:created_at]).strftime("%-d %B %Y")
 end
+
+def tils
+  blk = -> { @items.select { |item| item[:kind] == 'til' } }
+  if @items.frozen?
+    @article_items ||= blk.call
+  else
+    blk.call
+  end
+end
+
+def sorted_tils
+  blk = -> { tils.sort_by { |a| attribute_to_time(a[:created_at]) }.reverse }
+  if @items.frozen?
+    @sorted_til_items ||= blk.call
+  else
+    blk.call
+  end
+end
+
+def title_tag item
+  if item[:title]
+    "#{item[:title]} &middot; Sam Starling"
+  else
+    "Sam Starling"
+  end
+end
